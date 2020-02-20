e262904ca7553c5caba20dbbddaa4a71b6bf3d52
Aaron <asvaaron@gmail.com>
2020-02-19 20:58:51 -0600

    #3:Create appointment model view controller:Changes in seeds file
    Add additional User
    Change start and end time on the appointments
    Add the appointments to the first user

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,22 +13,40 @@ User.create!([
                     date_of_birth: "2019-07-11 00:00:00"
                  }
              ])
+# Create User
+User.create!([
+                 {
+                     email: "developer@developer.com",
+                     password: "Developer123",
+                     password_confirmation: "Developer123",
+                     reset_password_token: nil,
+                     reset_password_sent_at: nil,
+                     remember_created_at: nil,
+                     first_name: "Luis Carlos",
+                     last_name: "Monge Artavia",
+                     country: "CR",
+                     date_of_birth: "2019-07-11 00:00:00"
+                 }
+             ])
 # Create Appointments
 Appointment.create!([
                         {
-                            type_of: "lala",
-                            address: "ds",
+                            type_of: "Yoga",
+                            address: "San Jose, CR",
                             start_time: DateTime.now + 1,
-                            end_time: DateTime.now + 145
+                            end_time: DateTime.now + 1 + (1.0/24)
                         },
                         {
-                            type_of: "dsd",
-                            address: "dselas",
-                            start_time: DateTime.now + 1 ,
-                            end_time: DateTime.now + 180
+                            type_of: "Karate",
+                            address: "Heredia, CR",
+                            start_time: DateTime.now + 5 ,
+                            end_time: DateTime.now + 5 + (2.0/24)
                         },
                     ])
 
 
-# Add Apointments 1 to User
+# Add Appointment 1 to User 1
 User.first.appointments << Appointment.first
+# Add Appointment 2 to User 1
+User.first.appointments << Appointment.second
+
